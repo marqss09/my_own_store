9358dc375e45e002b9ecd1718b30b619bbec29b5
marqss09 <marek.golabek92@gmail.com>
2026-06-27 23:54:59 +0200

    Add prisma set up and test it with seed function

---
+++ prisma/migrations/20260627190410/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
